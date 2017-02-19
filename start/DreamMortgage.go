4f89eb66d995970c67fc717ce7a7dfd280d9c0d5
rohanadalvi <rohan.dalvi@capgemini.com>
2017-02-19 01:07:14 -0600

    upd smrt cntrct, mrtg prtyown, stg & index

--- start/DreamMortgage.go
+++ start/DreamMortgage.go
@@ -7,6 +7,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"encoding/json"
 )
@@ -77,8 +78,11 @@ type Mortgage struct {
 //==============================================================================================================================
 
 type mortgage_portfolio struct {
-	MortgageNumbers []int    `json:"MortgageNumbers"`
-	CustomerNames   []string `json:"CustomerNames"`
+	MortgageNumbers     			  []int    `json:"MortgageNumbers"`
+	CustomerNames       			  []string `json:"CustomerNames"`
+	MortgageStages      			  []string `json:"MortgageStages"`
+	ConformedMortgages				  []bool   `json:"ConformedMortgages"`
+	MortgagePropertyOwnerships  []string `json:"MortgagePropertyOwnerships"`
 }
 // ============================================================================================================================
 // Main
@@ -93,10 +97,8 @@ func main() {
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
-  // initialize the Mortgage number to 1000000
+  // initialize the Mortgage number
 	var mortgages mortgage_portfolio
-	mortgages.MortgageNumbers = []int{1000000}
-	mortgages.CustomerNames   = []string{""}
 	bytes, err := json.Marshal(mortgages)
 	if err != nil {
 		 return nil, errors.New("Error creating Mortgage Portfolio record")
@@ -162,25 +164,42 @@ func (t *SimpleChaincode) create_mortgage_application(stub shim.ChaincodeStubInt
  		}
 
 		// Generate Unique mortgage number and append to Mortgage portfolio
-		mortgage.MortgageNumber = mortgages.MortgageNumbers[len(mortgages.MortgageNumbers)-1]+1
-	  mortgages.MortgageNumbers = append(mortgages.MortgageNumbers,mortgage.MortgageNumber)
-	  mortgages.CustomerNames   = append(mortgages.CustomerNames,mortgage.CustomerName)
+		if len(mortgages.MortgageNumbers) > 0 {
+			mortgage.MortgageNumber = mortgages.MortgageNumbers[len(mortgages.MortgageNumbers)-1]+1
+		}else{
+			mortgage.MortgageNumber =1000001
+		}
 
-    //Store Mortgage in blockchain
+    //setting default values.
+		mortgage.MortgageStage="Pending-Bank:"
+		mortgage.ConformedMortgage=false
+		mortgage.MortgagePropertyOwnership="NOT_ACCQUIRED"
+
+	  mortgages.MortgageNumbers             = append(mortgages.MortgageNumbers,mortgage.MortgageNumber)
+	  mortgages.CustomerNames               = append(mortgages.CustomerNames,mortgage.CustomerName)
+		mortgages.MortgageStages              = append(mortgages.MortgageStages,mortgage.MortgageStage)
+		mortgages.ConformedMortgages          = append(mortgages.ConformedMortgages,mortgage.ConformedMortgage)
+		mortgages.MortgagePropertyOwnerships  = append(mortgages.MortgagePropertyOwnerships,mortgage.MortgagePropertyOwnership)
+
+    // Update Mortgage data into bytes.
 		mortgagebytes, err := json.Marshal(mortgage)
 		if err != nil {
 			 return nil, errors.New("Error in Marshalling New Mortgage record")
-		 }
-		err = stub.PutState(string(mortgage.MortgageNumber),mortgagebytes)
-    if err != nil {
-        return nil, err
-    }
+		}
 
-    //Store current Mortgage Portfolio in blockchain.
+    //package updated Mortgage Portfolio data into bytes.
 		bytes, err = json.Marshal(mortgages)
 		if err != nil {
 			 return nil, errors.New("Add to Mortgage Portfolio record")
-		 }
+		}
+
+    //Store Mortgage in blockchain
+		err = stub.PutState(string(mortgage.MortgageNumber),mortgagebytes)
+	  if err != nil {
+	      return nil, err
+	  }
+
+		//Store updated Mortgage Portfolio in blockchain
 		err = stub.PutState("mortgages", bytes)
 
     return nil, nil
@@ -190,8 +209,11 @@ func (t *SimpleChaincode) modify_mortgage(stub shim.ChaincodeStubInterface, args
     // Variable declaration
 	  var mortgage Mortgage
 		var currentmortgage Mortgage
+		var mortgages mortgage_portfolio
     var err error
-		var mortgagebytes []byte
+		var mortgagebytes, bytes []byte
+		var amountDisbursed bool
+		var counter, value, Ratio_1, Ratio_2, Ratio_3, Rating_Ratio int
 
 		//Logging
     fmt.Println("running modify_mortgage()")
@@ -200,6 +222,7 @@ func (t *SimpleChaincode) modify_mortgage(stub shim.ChaincodeStubInterface, args
     if len(args) != 1 {
         return nil, errors.New("Incorrect number of arguments. Expecting one JSON object to create mortgage application")
     }
+
 		//Assign JSON input and convert it to bytes
 		mortgage_json := args[0]
     err = json.Unmarshal([]byte(mortgage_json), &mortgage)
@@ -207,7 +230,7 @@ func (t *SimpleChaincode) modify_mortgage(stub shim.ChaincodeStubInterface, args
 			  return nil, errors.New("error while Unmarshalling mortgage json object")
 		}
 
-		//Get latest mortgages porfolio in blockchain and assign it to variable array
+		//Get latest mortgage in blockchain and assign it to variable array
 		mortgagebytes, err = stub.GetState(string(mortgage.MortgageNumber))
 		if err != nil {
  			  return nil, errors.New("error while fetching mortgage number")
@@ -224,16 +247,164 @@ func (t *SimpleChaincode) modify_mortgage(stub shim.ChaincodeStubInterface, args
 			  return nil, errors.New("error while Unmarshalling mortgage json object")
 		}
 
+    // smart contract fields
+		// Update Mortgage Stage.
+		if strings.ToUpper(currentmortgage.MortgageStage)== "APPROVED:" && currentmortgage.Ownershipcost > 0 {
+			 currentmortgage.GrantedLoanAmount = currentmortgage.Ownershipcost
+			 currentmortgage.MortgageStage="Disbursed:"
+			 amountDisbursed=true
+		}else{
+			 amountDisbursed=false
+			 if (strings.ToUpper(currentmortgage.MortgageStage)== "DISBURSED:READY TO PURCHASE" || strings.ToUpper(currentmortgage.MortgageStage)== "DISBURSED:READY TO SELL") && mortgage.Ownershipcost > 0 {
+				  currentmortgage.MortgageStage="Disbursed:Sold"
+			 }
+		}
 
-    //Store Mortgage in blockchain
+    // Update Mortgage Property Ownership
+     if amountDisbursed  {
+			  currentmortgage.MortgagePropertyOwnership="LENDING_BANK"
+		 }else if strings.ToUpper(currentmortgage.MortgageStage)== "DISBURSED:READY TO PURCHASE" && mortgage.Ownershipcost > 0 {
+			  currentmortgage.MortgagePropertyOwnership="GSE"
+		 }else if strings.ToUpper(currentmortgage.MortgageStage)== "DISBURSED:READY TO SELL" && mortgage.Ownershipcost > 0 {
+			  currentmortgage.MortgagePropertyOwnership="PARTNER_BANK"
+		 } else if strings.Index(strings.ToUpper(currentmortgage.MortgageStage),"DISBURSED:") < 0 {
+			 currentmortgage.MortgagePropertyOwnership="NOT_ACCQUIRED"
+		 }
+
+		//Calculate RemainingMortgageAmount
+		if strings.Index(strings.ToUpper(currentmortgage.MortgageStage),"DISBURSED:") < 0 {
+			  if currentmortgage.GrantedLoanAmount > 0 {
+			     currentmortgage.RemainingMortgageAmount = currentmortgage.GrantedLoanAmount
+				 }else{
+					 currentmortgage.RemainingMortgageAmount  = currentmortgage.ReqLoanAmount
+				 }
+		} else if amountDisbursed {
+			  currentmortgage.RemainingMortgageAmount = currentmortgage.GrantedLoanAmount
+		} else if (currentmortgage.RemainingMortgageAmount - currentmortgage.LastPaymentAmount) > 0 {
+		    currentmortgage.RemainingMortgageAmount = currentmortgage.RemainingMortgageAmount - currentmortgage.LastPaymentAmount
+		} else {
+			  currentmortgage.RemainingMortgageAmount=0
+		}
+
+    // if customer pays out property is moved back to customer.
+		if currentmortgage.RemainingMortgageAmount <=0 {
+			 currentmortgage.MortgagePropertyOwnership="CUSTOMER"
+		}
+
+		// Calculate Risk Classification.
+			if currentmortgage.RemainingMortgageAmount > 0 && currentmortgage.FinancialWorth > 0 && currentmortgage.CreditScore > 0 && currentmortgage.PropertyValuation > 0 {
+			   switch {
+			   case  currentmortgage.PropertyValuation*100/currentmortgage.RemainingMortgageAmount > 75:
+				Ratio_1 = 100
+			   case  currentmortgage.PropertyValuation*100/currentmortgage.RemainingMortgageAmount > 50 :
+				Ratio_1 = 75
+			   case  currentmortgage.PropertyValuation*100/currentmortgage.RemainingMortgageAmount > 25 :
+				Ratio_1 = 50
+			   default :
+				Ratio_1 = 25
+			   }
+		           switch {
+			   case  currentmortgage.FinancialWorth*100/currentmortgage.RemainingMortgageAmount > 75:
+				Ratio_2 = 100
+			   case  currentmortgage.FinancialWorth*100/currentmortgage.RemainingMortgageAmount > 50 :
+				Ratio_2 = 75
+			   case  currentmortgage.FinancialWorth*100/currentmortgage.RemainingMortgageAmount > 25 :
+				Ratio_2 = 50
+			   default :
+				Ratio_2 = 25
+			   }
+			   switch {
+			   case  currentmortgage.CreditScore > 700:
+				Ratio_3 = 100
+			   case  currentmortgage.CreditScore > 500 :
+				Ratio_3 = 75
+			   case  currentmortgage.CreditScore > 250 :
+				Ratio_3 = 50
+			   default :
+				Ratio_3 = 25
+			   }
+			   Rating_Ratio = (Ratio_1 + Ratio_2 + Ratio_3) / 3
+			   switch {
+			   case Rating_Ratio > 75:
+				currentmortgage.RiskClassification = "A"
+			   case Rating_Ratio > 50 :
+				currentmortgage.RiskClassification = "B"
+			   case Rating_Ratio > 25 :
+				currentmortgage.RiskClassification = "C"
+			   default :
+				currentmortgage.RiskClassification = "D"
+			   }
+			}else {
+			     currentmortgage.RiskClassification=""
+			}
+			switch currentmortgage.RiskClassification{
+			    case "A":
+			         currentmortgage.RiskAdjustedReturn=currentmortgage.RateofInterest
+			    case "B":
+			         currentmortgage.RiskAdjustedReturn=currentmortgage.RateofInterest*3/4
+			    case "C":
+			         currentmortgage.RiskAdjustedReturn=currentmortgage.RateofInterest*2/4
+			    case "D":
+			         currentmortgage.RiskAdjustedReturn=currentmortgage.RateofInterest*1/4
+			    default :
+			         currentmortgage.RiskAdjustedReturn=0
+			}
+			// Calculate Expected Annual CashFlow.
+			if currentmortgage.MortgageDuration > 365 {
+			   currentmortgage.ExpectedAnnualCashflow=currentmortgage.RemainingMortgageAmount/currentmortgage.MortgageDuration*365
+			}else {
+			   currentmortgage.ExpectedAnnualCashflow=currentmortgage.RemainingMortgageAmount
+			}
+			// Calculate if conformed currentmortgage.
+			if (currentmortgage.RiskClassification=="A" || currentmortgage.RiskClassification=="B" || currentmortgage.RiskClassification=="C") && currentmortgage.RemainingMortgageAmount <= 424100  && strings.Contains(strings.ToUpper(currentmortgage.MortgageStage),strings.ToUpper("Disbursed:"))  {
+			   currentmortgage.ConformedMortgage=true
+			}else{
+			   currentmortgage.ConformedMortgage=false
+			}
+
+			//Get latest mortgages porfolio in blockchain and assign it to variable array
+	 	 bytes, err = stub.GetState("mortgages")
+	 	 if err != nil {
+	 			 return nil, errors.New("error while retrieving mortgage portfolio json object")
+	 	 }
+	 	 err = json.Unmarshal(bytes,&mortgages)
+	 	 if err != nil {
+	 			 return nil, errors.New("error while Unmarshalling mortgages for new mortgage number")
+	 	 }
+
+
+    // identify place to update mortgage portfolio
+		 for counter, value = range mortgages.MortgageNumbers {
+				 if value == currentmortgage.MortgageNumber {
+						break
+			 }
+		 }
+
+     // Update mortgage portfolio with new details
+		 mortgages.CustomerNames[counter]               = currentmortgage.CustomerName
+		 mortgages.MortgageStages[counter]              = currentmortgage.MortgageStage
+     mortgages.ConformedMortgages[counter]          = currentmortgage.ConformedMortgage
+		 mortgages.MortgagePropertyOwnerships[counter]  = currentmortgage.MortgagePropertyOwnership
+
+		 //package updated Mortgage Portfolio data into bytes.
+	 		bytes, err = json.Marshal(mortgages)
+	 		if err != nil {
+	 			 return nil, errors.New("Add to Mortgage Portfolio record")
+	 		}
+
+		//Store updated Mortgage data in blockchain
 		mortgagebytes, err = json.Marshal(currentmortgage)
 		if err != nil {
 			 return nil, errors.New("Error in Marshalling New Mortgage record")
 		 }
+
 		err = stub.PutState(string(currentmortgage.MortgageNumber), mortgagebytes)
     if err != nil {
         return nil, err
     }
+
+		//Store updated Mortgage Portfolio in blockchain
+		err = stub.PutState("mortgages", bytes)
 
     return nil, nil
 }
